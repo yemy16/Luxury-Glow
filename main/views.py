b4de659d0d12f9629e6f0e73b506f28d0031295c
yemimaclra <yemimanainggolan0@gmail.com>
2024-09-16 15:11:13 +0700

    Melakukan tugas 3

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render, redirect
 from main.forms import ProductForm
 from main.models import Product
@@ -22,3 +24,19 @@ def create_product_entry(request):
 
     context = {'form': form}
     return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
