f05c0fdb736577b6e03230fc7c2c64b3e89cedd6
yemimaclra <yemimanainggolan0@gmail.com>
2024-09-16 19:33:32 +0700

    Merubah isi fields

--- main/models.py
+++ main/models.py
@@ -1,12 +1,12 @@
-import uuid
 from django.db import models
+import uuid
+from django.core.validators import MinValueValidator, MaxValueValidator  # Import validator
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=20, decimal_places=2)
+    price = models.IntegerField() 
     description = models.TextField()
-    image = models.ImageField()
     shade_name = models.CharField(max_length=100)
     stock_quantity = models.IntegerField(default=0)
 
