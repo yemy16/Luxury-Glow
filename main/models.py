c492a553bd78bff9b52046cc0dd7001da6f0a8ff
yemimaclra <yemimanainggolan0@gmail.com>
2024-09-16 08:48:49 +0700

    Melakukan perubahan pada models

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
+import uuid
 from django.db import models
 
 class LuxuryGlow(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=20, decimal_places=2)
     description = models.TextField()
