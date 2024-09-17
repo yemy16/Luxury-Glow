7704c2765198924d7f1886f1b483e403321a114c
yemimaclra <yemimanainggolan0@gmail.com>
2024-09-17 14:36:26 +0700

    Progress Tugas 3

--- Luxury_Glow/settings.py
+++ Luxury_Glow/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://yemima-clara31-luxuryglow.pbp.cs.ui.ac.id", "https://yemima-clara31-luxuryglow.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://yemima-clara31-luxuryglow.pbp.cs.ui.ac.id/", "https://yemima-clara31-luxuryglow.pbp.cs.ui.ac.id"]
 
