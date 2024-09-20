d99fb00d3aafbe0fa4a30f3d779b7dad5eb6e51c
yemimaclra <yemimanainggolan0@gmail.com>
2024-09-20 18:42:29 +0700

    Membenarkan error

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import Product
 
-class ProductForm(ModelForm):
+class ProductEntryForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description","shade_name","stock_quantity"]
