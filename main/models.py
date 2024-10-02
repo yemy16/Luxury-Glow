6b7af91eb083acc9156e4331c2d1e4dcb0d0d316
yemimaclra <yemimanainggolan0@gmail.com>
2024-10-02 22:14:00 +0700

    Progress Tugas 6

--- main/models.py
+++ main/models.py
@@ -7,10 +7,12 @@ class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.IntegerField() 
+    price = models.IntegerField(default=0)
     description = models.TextField()
     shade_name = models.CharField(max_length=100)
     stock_quantity = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='product/', blank=True, null=True)
+    image_url = models.URLField(max_length=200, default='http://example.com/default_image.jpg')
 
     def __str__(self):
         return self.name
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,27 @@
-from django.urls import path
-from main.views import show_main, create_product_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+from main.views import (
+    show_main,
+    create_product_entry,
+    edit_product,
+    delete_product,
+    show_xml,
+    show_json,
+    show_xml_by_id,
+    show_json_by_id,
+    register,
+    login_user,
+    logout_user,
+)
+
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product-entry', create_product_entry, name='create_product_entry'),
+    path('edit-product/<uuid:id>', edit_product, name='edit_product'),
+    path('delete-product/<uuid:id>', delete_product, name='delete_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
@@ -13,3 +30,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
