63af3078a7d3e027b3f61c0914be2d7aeb03e00b
yemimaclra <yemimanainggolan0@gmail.com>
2024-10-05 17:30:33 +0700

    Progress Tugas 6

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,33 @@
 from django.forms import ModelForm
-from .models import Product
+from main.models import Product
+from django.utils.html import strip_tags
 
 class ProductEntryForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description", "shade_name", "stock_quantity"]
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        return strip_tags(name)
+    
+    def clean_price(self):
+        price = self.cleaned_data["price"]
+        return strip_tags(price)
+    
+    def clean_description(self):
+        description = self.cleaned_data["description"]
+        return strip_tags(description)
+    
+    def clean_shade_name(self):
+        shade_name = self.cleaned_data["shade_name"]
+        return strip_tags(shade_name)
+    
+    def stock_quantity(self):
+        stock_quantity = self.cleaned_data["stock_quantity"]
+        return strip_tags(stock_quantity)
+
+
+
+
+
--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 import datetime
-from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
@@ -7,19 +6,20 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
 from main.forms import ProductEntryForm
 from main.models import Product
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.filter(user=request.user)
 
     context = {
         'app' : 'Luxury Glow',
         'name': request.user.username,
         'kelas': 'PBP B',
-        'product_entries' : product_entries,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -38,11 +38,11 @@ def create_product_entry(request):
     return render(request, "create_product_entry.html", context)
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -67,19 +67,22 @@ def register(request):
 
 def login_user(request):
    if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+       form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
+       if form.is_valid():
+           user = form.get_user()
+           login(request, user)
+           response = HttpResponseRedirect(reverse("main:show_main"))
+           response.set_cookie('last_login', str(datetime.datetime.now()))
+           return response
+       else:
+           messages.error(request, "Invalid username or password. Please try again.")
 
    else:
-      form = AuthenticationForm(request)
+       form = AuthenticationForm(request)
    context = {'form': form}
    return render(request, 'login.html', context)
+
 
 def logout_user(request):
     logout(request)
@@ -109,3 +112,25 @@ def delete_product(request, id):
     product.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    price = strip_tags(request.POST.get("price"))
+    description = strip_tags(request.POST.get("description"))
+    shade_name = strip_tags(request.POST.get("shade_name"))
+    stock_quantity = strip_tags(request.POST.get("stock_quantity"))
+    user = request.user
+
+    new_product = Product(
+        name=name, 
+        price=price,
+        description=description,
+        shade_name=shade_name,
+        stock_quantity=stock_quantity,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
